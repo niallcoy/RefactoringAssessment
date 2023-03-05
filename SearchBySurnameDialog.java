6e58ccbbaf076e6ea11b7104034c7f4c0713bded
Niall <niallcoyr@gmail.com>
2023-03-05 22:45:24 +0000

    search by Surname Redone to fix it i think

--- SearchBySurnameDialog.java
+++ SearchBySurnameDialog.java
@@ -1,3 +1,9 @@
+/*
+ * 
+ * This is a dialog for searching Employees by their surname.
+ * 
+ * */
+
 import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -13,13 +19,30 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.border.EtchedBorder;
 
-public abstract class SearchBySurnameDialog extends JDialog implements ActionListener {
-    protected JTextField searchField;
-    protected JButton searchButton;
-    protected JButton cancelButton;
+public abstract class SearchBySurnameDialog extends JDialog implements ActionListener{
+	EmployeeDetails parent;
+	JButton search, cancel;
+	JTextField searchField;
+	// constructor for search by surname dialog
+	public SearchBySurnameDialog(EmployeeDetails parent) {
+		setTitle("Search by Surname");
+		setModal(true);
+		this.parent = parent;
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-    public SearchBySurnameDialog(EmployeeDetails parent, String title) {
-        super(parent, title, true);
+		JScrollPane scrollPane = new JScrollPane();
+		setContentPane(scrollPane);
+
+		getRootPane().setDefaultButton(search);
+		
+		setSize(500, 190);
+		setLocation(350, 250);
+		setVisible(true);
+	}// end SearchBySurnameDialog
+	
+	// initialize search container
+	public void searchPane() {
+		
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
         Container contentPane = getContentPane();
@@ -30,47 +53,72 @@ public abstract class SearchBySurnameDialog extends JDialog implements ActionLis
 
         pack();
         setLocationRelativeTo(parent);
-    }
-
-     JPanel createTitlePanel() {
+//		JPanel searchPanel = new JPanel(new GridLayout(3,1));
+//		JPanel textPanel = new JPanel();
+//		JPanel buttonPanel = new JPanel();
+//		JLabel searchLabel;
+//
+//		searchPanel.add(new JLabel("Search by Surname"));
+//	
+//		textPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+//		textPanel.add(searchLabel = new JLabel("Enter Surname:"));
+//		searchLabel.setFont(this.parent.font1);
+//		textPanel.add(searchField = new JTextField(20));
+//		searchField.setFont(this.parent.font1);
+//		searchField.setDocument(new JTextFieldLimit(20));
+//
+//		buttonPanel.add(search = new JButton("Search"));
+//		search.addActionListener(this);
+//		search.requestFocus();
+//		
+//		buttonPanel.add(cancel = new JButton("Cancel"));
+//		cancel.addActionListener(this);
+//		
+//		searchPanel.add(textPanel);
+//		searchPanel.add(buttonPanel);
+//
+//		return searchPanel;
+	}// end searchPane
+	
+	JPanel createTitlePanel() {
         JPanel panel = new JPanel();
         panel.add(new JLabel(getTitle()));
         return panel;
-    }
-
-     JPanel createSearchPanel() {
+	}
+	
+	JPanel createSearchPanel() {
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
         panel.add(new JLabel(getSearchLabel()));
         panel.add(searchField = new JTextField(20));
         searchField.setDocument(new JTextFieldLimit(20));
         return panel;
-    }
-
-     abstract String getSearchLabel();
-
-    protected JPanel createButtonPanel() {
+	}
+	abstract String getSearchLabel();
+	JPanel createButtonPanel() {
         JPanel panel = new JPanel();
-        panel.add(searchButton = new JButton(getSearchButtonLabel()));
-        searchButton.addActionListener(this);
-        searchButton.requestFocus();
-        panel.add(cancelButton = new JButton(getCancelButtonLabel()));
-        cancelButton.addActionListener(this);
+        panel.add(search = new JButton(getSearchButtonLabel()));
+        search.addActionListener(this);
+        search.requestFocus();
+        panel.add(cancel = new JButton(getCancelButtonLabel()));
+        cancel.addActionListener(this);
         return panel;
     }
+	  abstract String getSearchButtonLabel();
+      abstract String getCancelButtonLabel();
+      abstract void performSearch();
 
-     abstract String getSearchButtonLabel();
-
-     abstract String getCancelButtonLabel();
-
-     abstract void performSearch();
-
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == searchButton) {
-            performSearch();
-            dispose();
-        } else if (e.getSource() == cancelButton) {
-            dispose();
-        }
-    }
-}
+	// action listener for save and cancel button
+	public void actionPerformed(ActionEvent e) {
+		// if option search, search for Employee
+		if(e.getSource() == search){
+			this.parent.searchBySurnameField.setText(searchField.getText());
+			// search Employee by surname
+			this.parent.searchEmployeeBySurname();
+			dispose();// dispose dialog
+		}// end if
+		// else dispose dialog
+		else if(e.getSource() == cancel)
+			dispose();// dispose dialog
+	}// end actionPerformed
+}// end class SearchBySurnameDialog
